e6dbb95adb20a8764110996f12f667c8a7b55ddb
Devin Ivy <devin@bigroomstudios.com>
2021-02-23 01:56:10 -0500

    Initial commit, playing with API ideas

---
+++ test/index.js
@@ -0,0 +1,78 @@
+'use strict'
+
+const crypto = require('crypto')
+const SecretStack = require('secret-stack')
+const Lifecycle = require('..');
+
+(() => {
+  const hash = (s) => crypto.createHash('sha256').update(s).digest()
+
+  const pluginA = {
+    name: 'plugin-a',
+    init (api) {
+      const { start, readyable, cb } = Lifecycle(api)
+      const ready = readyable()
+
+      start(() => {
+        const readyCb = cb(ready)
+        setTimeout(() => readyCb(new Error('d\'oh!')), 1000)
+      })
+
+      return { ready }
+    }
+  }
+
+  const pluginB = {
+    name: 'plugin-b',
+    init (api) {
+      const { readyable, start, wait, sync } = Lifecycle(api)
+      const someFuncReady = readyable()
+
+      start(() => {
+        wait(api.pluginA.ready, someFuncReady)
+
+        // wait(someFuncReady, api.pluginA.ready);
+      })
+
+      return {
+        ready: someFuncReady,
+        someFunc: sync(someFuncReady, () => 10)
+      }
+    }
+  }
+
+  const pluginC = {
+    name: 'plugin-c',
+    init (api) {
+      const { start, readyable, async, wait } = Lifecycle(api)
+      const someOtherFuncReady = readyable()
+
+      start(() => {
+        wait(api.pluginB.someFunc.ready, someOtherFuncReady)
+      })
+
+      // run(someOtherFuncReady, console.log);
+
+      return {
+        someOtherFunc: async(someOtherFuncReady, (cb) => cb(null, api.pluginB.someFunc()))
+      }
+    }
+  }
+
+  const create = SecretStack({ appKey: hash('secret-stack-lifecycle-testing') })
+    .use(pluginA)
+    .use(pluginB)
+    .use(pluginC)
+
+  const app = create()
+  const { run, ready } = Lifecycle(app)
+
+  // run(app.pluginA.ready, console.log);
+  // run(app.pluginA.ready, console.log);
+  // app.pluginC.someOtherFunc(() => {
+  //   console.log(null)
+  //   app.pluginC.someOtherFunc(console.log)
+  // })
+
+  run(ready, console.log)
+})()
